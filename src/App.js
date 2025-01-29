fed0e354bdfbfc8a21f3f00c273be4fd8caf6bb5
Shiqi <450008475@qq.com>
2025-01-28 22:45:24 -0600

    test1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h2>Test Codepipeline Successfully!</h2>
-      <h2>Test triggering cloudfront to update</h2>
-      <h2>process</h2>
+      <h2>Test triggering cloudfront to update 2</h2>
     </div>
   );
 }
