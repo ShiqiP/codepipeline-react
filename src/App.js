fe46830486f8beaa3ec6fc666280e59209210cb8
Shiqi <450008475@qq.com>
2025-01-28 22:23:01 -0600

    test:triggercloudfront

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h2>Test Codepipeline Successfully!</h2>
+      <h2>Test triggering cloudfront to update</h2>
     </div>
   );
 }
